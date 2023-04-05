48fc31f2f59073c085d3e213f66143711e8154dd
Aleks Kamarski <a.kamarski@devision.com>
2023-04-05 21:36:35 +0300

    cleanup: Applied optimizations and refactoring for better readability, reusability and maintainability

--- src/main/java/org/demo/Context.java
+++ src/main/java/org/demo/Context.java
@@ -2,41 +2,39 @@ package org.demo;
 
 import static java.lang.Boolean.*;
 import static java.util.Optional.ofNullable;
+import static org.demo.model.InstallmentType.DAYS;
+import static org.demo.model.InstallmentType.MONTHS;
+import static org.demo.model.InstallmentType.WEEKS;
 
+import java.util.EnumMap;
+import java.util.Map;
+import org.demo.model.InstallmentType;
 import org.demo.model.SortParams;
 import org.demo.model.SortParams.SortBy;
 import org.demo.model.SortParams.SortDirection;
 
 public class Context {
 
-  private String filePath;
+  private final String filePath;
   private boolean displayShort;
-  private int installmentTypeDays;
-  private int installmentTypeWeeks;
-  private int installmentTypeMonths;
-  private SortParams sortParams;
+  private final SortParams sortParams;
+  private final Map<InstallmentType, Integer> map;
 
   public Context() {
     this.filePath = System.getenv(EnvironmentVariables.FILE_PATH);
 
-    this.displayShort = parseBoolean(System.getenv(EnvironmentVariables.DISPLAY_SHORT_RESULT));
-
-    this.installmentTypeDays = ofNullable(System.getenv(EnvironmentVariables.INSTALLMENT_TYPE_DAYS)).map(
-        Integer::valueOf).orElse(365);
-
-    this.installmentTypeWeeks = ofNullable(System.getenv(EnvironmentVariables.INSTALLMENT_TYPE_WEEKS))
-        .map(Integer::valueOf).orElse(53);
-
-    this.installmentTypeMonths = ofNullable(System.getenv(EnvironmentVariables.INSTALLMENT_TYPE_MONTHS))
-        .map(Integer::valueOf).orElse(12);
+    ofNullable(System.getenv(EnvironmentVariables.DISPLAY_SHORT_RESULT))
+        .ifPresentOrElse(display -> this.displayShort = parseBoolean(display),
+            () -> this.displayShort = true);
 
     final SortBy sortBy = ofNullable(System.getenv(EnvironmentVariables.SORT_BY))
         .map(SortBy::valueOf).orElse(null);
-    final SortDirection sortOrder = ofNullable(System.getenv(EnvironmentVariables.SORT_ORDER))
+    final SortDirection sortOrder = ofNullable(System.getenv(EnvironmentVariables.SORT_DIRECTION))
         .map(SortDirection::valueOf).orElse(null);
     this.sortParams = new SortParams(sortBy, sortOrder);
-  }
 
+    this.map = initializeInstallmentMap();
+  }
 
   public String getFilePath() {
     return filePath;
@@ -46,31 +44,41 @@ public class Context {
     return displayShort;
   }
 
-  public int getInstallmentTypeDays() {
-    return installmentTypeDays;
-  }
-
-  public int getInstallmentTypeWeeks() {
-    return installmentTypeWeeks;
-  }
-
-  public int getInstallmentTypeMonths() {
-    return installmentTypeMonths;
-  }
-
   public SortParams getSortParams() {
     return sortParams;
+  }
+
+  public Map<InstallmentType, Integer> getMap() {
+    return map;
+  }
+
+  private Map<InstallmentType, Integer> initializeInstallmentMap() {
+    final Map<InstallmentType, Integer> map = new EnumMap<>(InstallmentType.class);
+    int installmentTypeDays = ofNullable(System.getenv(EnvironmentVariables.INSTALLMENT_TYPE_DAYS)).map(
+        Integer::valueOf).orElse(365);
+    int installmentTypeWeeks = ofNullable(System.getenv(EnvironmentVariables.INSTALLMENT_TYPE_WEEKS))
+        .map(Integer::valueOf).orElse(53);
+    int installmentTypeMonths = ofNullable(System.getenv(EnvironmentVariables.INSTALLMENT_TYPE_MONTHS))
+        .map(Integer::valueOf).orElse(12);
+
+    map.put(DAYS, installmentTypeDays);
+    map.put(WEEKS, installmentTypeWeeks);
+    map.put(MONTHS, installmentTypeMonths);
+
+    return map;
   }
 
   private static class EnvironmentVariables {
 
     private static final String FILE_PATH = "FILE_PATH";
     private static final String DISPLAY_SHORT_RESULT = "DISPLAY_SHORT_RESULT";
+
+    private static final String SORT_BY = "SORT_BY";
+    private static final String SORT_DIRECTION = "SORT_DIRECTION";
+
     private static final String INSTALLMENT_TYPE_DAYS = "INSTALLMENT_TYPE_DAYS";
     private static final String INSTALLMENT_TYPE_WEEKS = "INSTALLMENT_TYPE_WEEKS";
     private static final String INSTALLMENT_TYPE_MONTHS = "INSTALLMENT_TYPE_MONTHS";
-    private static final String SORT_BY = "SORT_BY";
-    private static final String SORT_ORDER = "SORT_ORDER";
   }
 
 }
--- src/main/java/org/demo/Program.java
+++ src/main/java/org/demo/Program.java
@@ -1,17 +1,11 @@
 package org.demo;
 
-import static org.demo.model.InstallmentType.DAYS;
-import static org.demo.model.InstallmentType.MONTHS;
-import static org.demo.model.InstallmentType.WEEKS;
-
-import java.util.EnumMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 import org.demo.file_factory.Handler;
 import org.demo.file_factory.HandlerImpl;
 import org.demo.file_factory.xlsx.XlsxFileOutputGenerator;
-import org.demo.model.InstallmentType;
 import org.demo.model.Result;
 import org.demo.model.ShortResult;
 
@@ -24,14 +18,15 @@ public class Program {
     final XlsxFileOutputGenerator xlsxFileOutputGenerator = new XlsxFileOutputGenerator();
     final RawDataParser rawDataParser = new RawDataParser();
     final CompoundInterestCalculationService calculationService = new CompoundInterestCalculationService();
-    final EnumMap<InstallmentType, Integer> map = new EnumMap<>(InstallmentType.class);
-    fillDefaultMapValues(map, context);
     final SortService sortService = new SortService();
-
-    final Handler handler = new HandlerImpl(xlsxFileOutputGenerator, rawDataParser, calculationService, map);
+    final Handler handler = new HandlerImpl(
+        xlsxFileOutputGenerator,
+        rawDataParser,
+        calculationService,
+        context.getMap());
 
     final FileInput fileInput = new FileInput(context.getFilePath(), fileExtension);
-    List<Result> results = sortService.sort(handler.handle(fileInput), context.getSortParams());
+    final List<Result> results = sortService.sort(handler.handle(fileInput), context.getSortParams());
 
     if (context.isDisplayShort()) {
       List<ShortResult> collect = results.stream().map(ShortResult::new).collect(Collectors.toList());
@@ -39,12 +34,6 @@ public class Program {
     } else {
       System.out.println(results);
     }
-  }
-
-  private static void fillDefaultMapValues(EnumMap<InstallmentType, Integer> map, Context context) {
-    map.put(DAYS, context.getInstallmentTypeDays());
-    map.put(WEEKS, context.getInstallmentTypeWeeks());
-    map.put(MONTHS, context.getInstallmentTypeMonths());
   }
 
   private static ExtensionType getFileExtension(String fileLocation) {
